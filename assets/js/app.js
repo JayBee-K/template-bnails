e3c82f970d3fd137e80eeb92005fff8f13ec922f
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2023-02-28 13:50:08 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,9 +54,36 @@
 		}
 	}
 
+	const handleSliderBlogs = function () {
+		if ($('#slider-blogs').length) {
+			new Swiper('#slider-blogs .swiper', {
+				speed: 400,
+				spaceBetween: 20,
+				slidesPerView: 3.25,
+				preloadImages: false,
+				effect: 'slide',
+				loop: true,
+				autoplay: {
+					delay: 8000,
+					disableOnInteraction: false,
+				},
+				pagination: {
+					el: "#slider-blogs .swiper-actions-pagination",
+					clickable: true,
+				},
+				navigation: {
+					nextEl: '#slider-blogs .swiper-actions_next',
+					prevEl: '#slider-blogs .swiper-actions_prev',
+				},
+			});
+		}
+	}
+
+
 	$(function () {
 		handleSliderHero();
 		handleSliderFeedback();
+		handleSliderBlogs();
 
 		$(window).resize(() => {
 			windowWidth = $(window).width();
